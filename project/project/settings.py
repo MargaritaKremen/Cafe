71dff13032f83584939a085983edeab97da77575
Margarita <kozlovamarharyta@gmail.com>
2023-12-06 20:27:24 +0000

    Додано .gitignore

--- project/project/settings.py
+++ project/project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_!i8ujm=opp0@t#x-*&yks)5lk(m8$#16rwsueo*0+e+w%zr_2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
